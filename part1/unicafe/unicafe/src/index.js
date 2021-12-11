6bb4c17b09e537ec0f6acec313ef07af54f0d66e
Ezequiel Gonzalez <zequi14@hotmail.com>
2021-12-11 16:54:30 +0100

    1.6 unicafe_paso1

---
+++ part1/unicafe/unicafe/src/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+
+const Button = ({onClick, buttonText}) =>{
+  return (
+          <button onClick ={onClick}>
+            {buttonText}
+          </button>
+    ) 
+}
+
+const Display = ({displayText, displayValue}) =>{
+  return (
+    <p>{displayText} {displayValue}</p>) 
+}
+
+const Title = ({text}) => text
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good +1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral +1)
+  }
+  const handleBadClick = () => {
+    setBad(bad +1)
+  }
+
+  return (
+    <div>
+      <p><Title text= 'give feedback'/></p>
+      <Button  onClick={handleGoodClick} buttonText='good'/>
+      <Button  onClick={handleNeutralClick} buttonText='neutral'/> 
+      <Button  onClick={handleBadClick} buttonText='bad'/> 
+      <p><Title text= 'statistics'/></p>
+      <Display displayText= 'good'  displayValue ={good}/>
+      <Display displayText= 'neutral'  displayValue ={neutral}/>
+      <Display displayText= 'bad' displayValue={bad}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
