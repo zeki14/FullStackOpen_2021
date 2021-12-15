3da46db6ae26e4875e816713b295e813d05da31f
Ezequiel Gonzalez <zequi14@hotmail.com>
2021-12-15 19:43:30 +0100

    2.4 courseinfo_paso9

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -1,42 +1,51 @@
 import React from 'react'
 
-const Course = (props) =>{
-    const { course } = props
-    console.log(course);
-    return(
-        <div>
-            <Header course={course}/>
-            <Content course={course}/>
-        </div>
-    )
-}
+    const Course = (props) =>{
+        const { course } = props
+        console.log(course);
+        return(
+            <div>
+                <Header course={course}/>
+                <Content course={course}/>
+                <Total course={course}/>
+            </div>
+        )
+    }
 
-
-const Header = ({ course }) => {
-    return (
-      <h1>{course.name}</h1>
-    )
-  }
-
+    const Header = ({ course }) => {
+        return (
+        <h1>{course.name}</h1>
+        )
+    }
   
-  const Content = (props) => {
-      const { course } = props
-    return (
-      <ul>
-          {course.parts.map(part => <Part key={part.id} course={part}/>)}
-         
-      </ul>
-    )
-  }
+    const Content = (props) => {
+        const { course } = props
+        return (
+        <ul>
+            {course.parts.map(part => <Part key={part.id} course={part}/>)}
+            
+        </ul>
+        )
+    }
 
-  const Part = ({course}) => {
-    return (
-      <li>{course.name}  {course.exercises}</li>    
-    )
-  }
+    const Part = ({course}) => {
+        return (
+        <li>{course.name}  {course.exercises}</li>    
+        )
+    }
 
-  
+    const Total = ({ course }) => {
 
+        const values = course.parts.map(part => {
+            return part.exercises;
+        })
+        let total =0;
+        for(let i =0; i < values.length; i++){
+            total += values[i]
+        }
+        return(
+          <p>Total of exercises {total}</p>
+        ) 
+      }
 
-  
-  export default Course;
+export default Course;
